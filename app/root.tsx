3d6844a1f18d0ed4d7b957dce479d0de5f910d1e
Andrew Marcuse <fileformat@gmail.com>
2024-07-29 15:40:06 -0400

    ADD: started with Bootstrap (w/o React-specific stuff)

--- app/root.tsx
+++ app/root.tsx
@@ -9,16 +9,19 @@ import "./tailwind.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+     <html lang="en" style={{"width":"100%","height":"100%"}}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body style={{ "width": "100%", "height": "100%" }}>
         {children}
         <ScrollRestoration />
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossOrigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossOrigin="anonymous"></script>       
         <Scripts />
       </body>
     </html>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,43 +6,40 @@ export const meta: MetaFunction = () => {
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
+import { PiAcornBold } from "react-icons/pi";
+import { LogoIcon } from "~/components/LogoIcon";
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main>
+      <nav className="navbar navbar-expand bg-light border-bottom">
+        <div className="container-fluid">
+          <LogoIcon style={{height:"24pt"}} />
+        <div className="btn-group" role="group" aria-label="Basic example">
+            <button type="button" className="btn btn-light border"><PiAcornBold/></button>
+            <button type="button" className="btn btn-light border">Middle</button>
+            <button type="button" className="btn btn-light border">Right</button>
+        </div>
+          <a className="navbar-brand" href="?x">Always expand</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarsExample02">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="?x">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="?x">Link</a>
+              </li>
+            </ul>
+            <form role="search">
+              <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+            </form>
+          </div>
+        </div>
+      </nav>
+    </main>
   );
 }
