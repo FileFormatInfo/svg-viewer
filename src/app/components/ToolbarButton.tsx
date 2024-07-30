757d0178a61d0538b59bd4659c399292bb0d5d71
Andrew Marcuse <fileformat@gmail.com>
2024-07-30 09:14:29 -0400

    UPDATE: tweaks to toolbar button to prevent suspense flash: next/link

--- src/app/components/ToolbarButton.tsx
+++ src/app/components/ToolbarButton.tsx
@@ -3,7 +3,7 @@ import { IconType } from "react-icons";
 
 import { Icon, IconButton } from "@chakra-ui/react";
 
-import { Link as NextLink } from "@chakra-ui/next-js";
+import Link from "next/link";
 
 interface IProps {
   ariaLabel: string;
@@ -23,15 +23,16 @@ function ToolbarButton({
   size,
 }: IProps) {
   return (
-    <NextLink href={href}>
+    <Link href={href}>
       <IconButton
         aria-label={ariaLabel}
+        as="div"
         icon={<Icon boxSize={boxSize} as={icon} />}
         isActive={isActive}
         size={size}
         title={ariaLabel}
       />
-    </NextLink>
+    </Link>
   );
 }
 
