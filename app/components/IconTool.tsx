8c276d4dd489e5dee71d3a80654413ebe8698310
Andrew Marcuse <fileformat@gmail.com>
2024-08-05 12:25:58 -0400

    UPDATE: fix icon toolbar svg->react attribute name error

--- app/components/IconTool.tsx
+++ app/components/IconTool.tsx
@@ -2,7 +2,7 @@
 import { IconBase, IconBaseProps } from "react-icons";
 
 const IconTool = (props: IconBaseProps) => (
-    <IconBase width="1em" height="1em" fill="currentColor" stroke="currentColor" stroke-width="0" viewBox="0 0 256 256" {...props}>
+    <IconBase width="1em" height="1em" fill="currentColor" stroke="currentColor" strokeWidth="0" viewBox="0 0 256 256" {...props}>
         <path stroke="none" d="M160.022 79.768h72c4.432 0 8 3.568 8 8v72c0 4.432-3.568 8-8 8h-72c-4.432 0-8-3.568-8-8v-72c0-4.432 3.568-8 8-8M81.07 103.693h48c4.432 0 8 3.568 8 8v48c0 4.432-3.568 8-8 8h-48c-4.432 0-8-3.568-8-8v-48c0-4.432 3.568-8 8-8m-56.845 24.023h24c4.432 0 8 3.568 8 8v24c0 4.432-3.568 8-8 8h-24c-4.432 0-8-3.568-8-8v-24c0-4.432 3.568-8 8-8" style={{"fill":"#000", "fillOpacity":"1"}} />
     </IconBase>
 );
