31cb90e10a96b18226b1fa00081859816d5ec602
Andrew Marcuse <fileformat@gmail.com>
2024-10-16 17:11:19 -0400

    Remove sleep call used during dev

--- app/routes/random[.]html.tsx
+++ app/routes/random[.]html.tsx
@@ -19,7 +19,7 @@ async function getRandomImage(hostname: string|null, zoom: string|null): Promise
     zoom = zoom || "max";
 
     try {
-        await new Promise(r => setTimeout(r, 2000))
+        //await new Promise(r => setTimeout(r, 2000))
         const resp = await fetch(`https://${hostname}/api/random.json?max=1`);
         const data = await resp.json();
 
